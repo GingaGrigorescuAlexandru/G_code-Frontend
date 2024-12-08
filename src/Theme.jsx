a0d80c2fd102c8db31ca2e608ead39bca668e650
ALEX13GBeb <167245668+ALEX13GBeb@users.noreply.github.com>
2024-12-08 22:08:08 +0200

    Pick and initialize a Pallete

---
+++ src/Theme.jsx
@@ -0,0 +1,20 @@
+import { createTheme } from '@material-ui/core/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#26a69a',
+      main: '#00796b',
+      dark: '#004d40',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#ec407a',
+      main: '#d81b60',
+      dark: '#ad1457',
+      contrastText: '#fff',
+    },
+  },
+});
+
+export default theme;
